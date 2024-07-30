be6151da7a3c43719a6b8f5da833b2b636b4d3b2
Nicholas Hieb <nicholashieb@icloud.com>
2024-07-30 18:12:50 -0500

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by Nicholas M Hieb on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
